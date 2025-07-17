217cad0a1717e9e598e5eec5b691f2286beddaf2
Kory <korymbuck@gmail.com>
2025-07-17 14:54:53 -0500

    Added RunItUp logo and updated manifest.json/sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,6 @@ const urlsToCache = [
   "/index.html",
   "/manifest.json",
   "/icons/icon-192.png",
-  "/icons/icon-512.png",
   "https://cdn.tailwindcss.com",
   "https://unpkg.com/vue@3/dist/vue.global.js",
 ];
@@ -22,40 +21,9 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
-        return response; // Cache hit - return response
-      }
-      return fetch(event.request).then((response) => {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
-
-        // IMPORTANT: Clone the response. A response is a stream
-        // and can only be consumed once. We must clone it so that
-        // the browser can consume the original response and we can consume the clone.
-        const responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-
         return response;
-      });
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+      }
+      return fetch(event.request);
     })
   );
 });
