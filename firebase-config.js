d1d12bcd4778cebeeaa9fc9fbc4613cb6201d412
Kory <korymbuck@gmail.com>
2025-07-24 13:09:37 -0500

    Added initial friend/social functions, will update later pt2

--- firebase-config.js
+++ firebase-config.js
@@ -1,30 +1,9 @@
-// RunItUp-main/firebase-config.js
+// Import the functions you need from the SDKs
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signOut,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  where,
-  getDocs,
-  deleteDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  writeBatch,
-  getDoc,
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-// YOUR WEB APP'S FIREBASE CONFIGURATION (PASTE IT HERE!)
-// This config should be copied directly from the Firebase Console (from Part 1, Step 3 above)
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDk-n02q6hW_41tCnLw4oaYYjEh3codxcw",
   authDomain: "runit-91e6f.firebaseapp.com",
@@ -32,29 +11,11 @@ const firebaseConfig = {
   storageBucket: "runit-91e6f.firebasestorage.app",
   messagingSenderId: "1098137720623",
   appId: "1:1098137720623:web:48a6cb82eb37a08fd6b3da",
-  measurementId: "G-4S4YFS4LZQ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services and export them
+// Initialize and export Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export {
-  onAuthStateChanged,
-  signOut,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  collection,
-  addDoc,
-  query,
-  where,
-  getDocs,
-  deleteDoc,
-  doc,
-  setDoc,
-  updateDoc,
-  writeBatch,
-  getDoc,
-};
