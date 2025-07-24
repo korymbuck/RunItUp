c24da1d0da4ae8df2eb5f7fd8fef14e4ac516249
Kory <korymbuck@gmail.com>
2025-07-24 13:14:37 -0500

    Added initial friend/social functions, will update later pt3

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-// Import the functions you need from the SDKs
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// Import from the full CDN URLs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDk-n02q6hW_41tCnLw4oaYYjEh3codxcw",
   authDomain: "runit-91e6f.firebaseapp.com",
   projectId: "runit-91e6f",
-  storageBucket: "runit-91e6f.firebasestorage.app",
+  storageBucket: "runit-91e6f.appspot.com",
   messagingSenderId: "1098137720623",
   appId: "1:1098137720623:web:48a6cb82eb37a08fd6b3da",
 };
